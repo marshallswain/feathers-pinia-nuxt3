50d06b81d001e3842acbe95f15eeb42e55abf795
Marshall Thompson <marshall@creativeideal.net>
2022-09-19 23:00:49 -0600

    feat add favicon

--- app.vue
+++ app.vue
@@ -1,9 +1,25 @@
+<script setup lang="ts">
+// https://v3.nuxtjs.org/api/composables/use-head
+useHead({
+  title: 'Feathers-Pinia',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'Feathers-Pinia Example App' },
+  ],
+  script: [],
+  link: [
+    { hid: 'favicon', rel: 'icon', type: 'image/svg', href: '/feathersjs.svg' },
+  ],
+  bodyAttrs: {},
+  htmlAttrs: {},
+})
+</script>
+
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
 
 <style>
