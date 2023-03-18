fe58f90cc622698b009211cc1cf5bd43c5a52a09
Marshall Thompson <marshall@creativeideal.net>
2023-03-17 23:39:48 -0600

    nested routing for /contacts

--- pages/app/contacts/[id].vue
+++ pages/app/contacts/[id].vue
@@ -1,21 +1,15 @@
 <script setup lang="ts">
+defineEmits(['openDrawer'])
 definePageMeta({
   layout: 'app',
 })
 </script>
 
 <template>
-  <DaisyFlex col items-center class="h-full gap-2 mx-auto container">
-    <DaisyDrawerLayout mobile class="w-full h-full m-6 gap-3">
-      <DaisyDrawerLayoutContent v-slot="{ openDrawer }" class="flex py-3">
-        <DaisyButton primary class="lg:hidden" @click="() => openDrawer()">
-          Open Contact List
-        </DaisyButton>
-      </DaisyDrawerLayoutContent>
-
-      <DaisyDrawer v-slot="{ closeDrawer }" class="rounded-box border border-base-200">
-        <ContactsMenu v-bind="{ closeDrawer }" />
-      </DaisyDrawer>
-    </DaisyDrawerLayout>
-  </DaisyFlex>
+  <div class="flex flex-col">
+    Contact Detail view
+    <DaisyButton primary class="lg:hidden" @click="$emit('openDrawer')">
+      Open Contact List
+    </DaisyButton>
+  </div>
 </template>
--- pages/app/contacts/new.vue
+++ pages/app/contacts/new.vue
@@ -1,38 +1,15 @@
 <script setup lang="ts">
+defineEmits(['openDrawer'])
 definePageMeta({
   layout: 'app',
 })
 </script>
 
 <template>
-  <DaisyFlex col items-center class="h-full gap-2 mx-auto container">
-    <DaisyDrawerLayout mobile class="w-full h-full m-6 gap-3">
-      <DaisyDrawerLayoutContent v-slot="{ openDrawer }" class="flex py-3">
-        <DaisyButton primary class="lg:hidden" @click="() => openDrawer()">
-          Open Contact List
-        </DaisyButton>
-      </DaisyDrawerLayoutContent>
-
-      <DaisyDrawer v-slot="{ closeDrawer }" class="rounded-box border border-base-200">
-        <DaisyMenu class="relative p-3 overflow-y-auto menu w-80 ">
-          <RouterLink v-slot="{ isActive, navigate }" to="/app/contacts/new" custom>
-            <DaisyButton :primary="isActive" :neutral="!isActive" @click="navigate">
-              Create Contact
-            </DaisyButton>
-          </RouterLink>
-
-          <DaisyMenuTitle class="mt-4 ">
-            <p>Select a Contact</p>
-          </DaisyMenuTitle>
-          <DaisyMenuItem @click="() => closeDrawer()">
-            <a>Menu Item</a>
-          </DaisyMenuItem>
-
-          <DaisyMenuItem @click="() => closeDrawer()">
-            <a>Menu Item</a>
-          </DaisyMenuItem>
-        </DaisyMenu>
-      </DaisyDrawer>
-    </DaisyDrawerLayout>
-  </DaisyFlex>
+  <div class="flex flex-col">
+    New Contact Form
+    <DaisyButton primary class="lg:hidden" @click="$emit('openDrawer')">
+      Open Contact List
+    </DaisyButton>
+  </div>
 </template>
