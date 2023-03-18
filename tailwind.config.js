d92d47dc722b4dc1104eb42eeae3eee4d8b08465
Marshall Thompson <marshall@creativeideal.net>
2023-03-17 23:41:42 -0600

    install iconify/tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
 
 export default {
   darkMode: 'class',
-  plugins: [require('daisyui')],
+  plugins: [
+    require('daisyui'),
+    addDynamicIconSelectors(),
+  ],
   content: ['node_modules/daisy-ui-kit/**/*.{vue,js}'],
   theme: {
     extend: {
