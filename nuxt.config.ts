49457a4f705a89358be22995b3541a318ddc5575
Marshall Thompson <marshall@creativeideal.net>
2023-03-20 01:12:03 -0600

    add nuxt devtools (commented out)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
+    // '@nuxt/devtools',
     '@pinia/nuxt',
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt',
