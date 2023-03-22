c81a65150bd560bf560eb957a9babb650ec7f02f
Marshall Thompson <marshall@creativeideal.net>
2023-03-21 19:28:56 -0600

    separate ContactsContextMenu component

---
+++ components/Contacts/ContextMenu.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineEmits(['openMenu'])
+</script>
+
+<template>
+  <DaisyDropdown end>
+    <DaisyButton is="label" ghost sm circle class="-m-2" tabindex="0">
+      <i class="icon-[feather--menu]" />
+    </DaisyButton>
+    <DaisyMenu tabindex="0" class="shadow dropdown-content bg-base-200 rounded-box">
+      <DaisyMenuItem>
+        <DaisyButton error class="w-32" @click="$emit('openMenu')">
+          <span>Delete</span>
+          <i class="icon-[feather--trash-2]" />
+        </DaisyButton>
+      </DaisyMenuItem>
+    </DaisyMenu>
+  </DaisyDropdown>
+</template>
