e4d8e4cda50de4718b1e2109d04ba9503805ceec
Marshall Thompson <marshall@creativeideal.net>
2023-03-24 07:22:07 -0600

    Show filter options menu in Contacts area

---
+++ components/Contacts/Menu/ContactsFilterMenu.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface Props {
+  itemsPerPage: number
+}
+const props = defineProps<Props>()
+const emit = defineEmits(['update:itemsPerPage'])
+
+const { itemsPerPage } = useVModels(props, emit)
+</script>
+
+<template>
+  <DaisyDropdown end>
+    <DaisyButton class="relative rounded-l-none">
+      <i class="icon-[feather--search] relative right-0.5" />
+      <i class="icon-[feather--chevron-down] absolute right-1" />
+    </DaisyButton>
+
+    <DaisyMenu tabindex="0" class="shadow dropdown-content bg-base-200 rounded-box p-2">
+      <DaisyMenuItem class="p-2">
+        <DaisyTextInput v-model.number="itemsPerPage" />
+      </DaisyMenuItem>
+      <DaisyMenuItem>
+        <span>Test</span>
+      </DaisyMenuItem>
+    </DaisyMenu>
+  </DaisyDropdown>
+</template>
---
+++ components/Contacts/Menu/ContactsMenuItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  contact: Record<string, any>
+  isActive: boolean
+}
+defineProps<Props>()
+defineEmits(['openMenu'])
+</script>
+
+<template>
+  <DaisyMenuItem>
+    <DaisyFlex :class="{ active: isActive }">
+      <span class="flex-grow">{{ contact.firstName }} {{ contact.lastName }}</span>
+      <ContactsContextMenu v-if="isActive" @open-menu="$emit('openMenu', $event)" />
+    </DaisyFlex>
+  </DaisyMenuItem>
+</template>
