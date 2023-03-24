76c8e375e6c3e54e3013ecb2c05403393e33f300
Marshall Thompson <marshall@creativeideal.net>
2023-03-24 07:21:02 -0600

    reorder hooks again

--- feathers-pinia-3/hooks/index.ts
+++ feathers-pinia-3/hooks/index.ts
@@ -1,19 +1,19 @@
 import { eventLocks } from './2-event-locks'
 import { handleFindSsr } from './5-handle-find-ssr'
-import { makeModelInstances } from './3-model-instances'
+import { makeModelInstances } from './4-model-instances'
 import { normalizeFind } from './6-normalize-find'
 import { patchDiffing } from './8-patch-diffs'
 import { setPending } from './1-set-pending'
 import { skipGetIfExists } from './7-skip-get-if-exists'
-import { syncStore } from './4-sync-store'
+import { syncStore } from './3-sync-store'
 
 export { syncStore, setPending, eventLocks, normalizeFind, skipGetIfExists, makeModelInstances }
 
 export const feathersPiniaHooks = () => [
   setPending(),
   eventLocks(),
-  makeModelInstances(),
   syncStore(),
+  makeModelInstances(),
   handleFindSsr(),
   normalizeFind(),
   skipGetIfExists(),
