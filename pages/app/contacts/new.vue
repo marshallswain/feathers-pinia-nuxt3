6d884205ed65930303de9b6318e3062005c6e174
Marshall Thompson <marshall@creativeideal.net>
2023-03-24 22:13:58 -0600

    Reset the New Contact from after submission

--- pages/app/contacts/new.vue
+++ pages/app/contacts/new.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { reset } from '@formkit/core'
+
 defineEmits(['openDrawer'])
 definePageMeta({
   layout: 'app',
@@ -8,6 +10,7 @@ const { api } = useFeathers()
 
 async function createContact(fields: any) {
   const result = await api.service('contacts').create(fields)
+  reset('contact-form')
   await navigateTo(`${result._id}`)
 }
 </script>
