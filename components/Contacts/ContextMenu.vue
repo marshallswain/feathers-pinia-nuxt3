99e72a4ba95a836bf402a154bbacba11ae1d9d76
Marshall Thompson <marshall@creativeideal.net>
2023-03-24 22:12:05 -0600

    allow opening contacts context menu

--- components/Contacts/ContextMenu.vue
+++ components/Contacts/ContextMenu.vue
@@ -4,12 +4,12 @@ defineEmits(['openMenu'])
 
 <template>
   <DaisyDropdown end>
-    <DaisyButton is="label" ghost sm circle class="-m-2" tabindex="0">
+    <DaisyButton is="label" ghost sm circle class="-m-2" tabindex="0" @click.stop>
       <i class="icon-[feather--menu]" />
     </DaisyButton>
     <DaisyMenu tabindex="0" class="shadow dropdown-content bg-base-200 rounded-box">
       <DaisyMenuItem>
-        <DaisyButton error class="w-32" @click="$emit('openMenu')">
+        <DaisyButton error class="w-32" @click.stop="$emit('openMenu')">
           <span>Delete</span>
           <i class="icon-[feather--trash-2]" />
         </DaisyButton>
